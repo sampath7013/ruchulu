43ca59587b44a4a9d30dbd8595742e8696da0061
sampath7013 <sampathkumarmuthyalapati@gmail.com>
2025-05-21 23:07:16 -0500

    first commit

--- src/suby/components/FirmCollections.jsx
+++ src/suby/components/FirmCollections.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { API_URL } from "../../api";
+import { API_URL } from "../api";
 import { Link } from "react-router-dom";
 
 const FirmCollections = () => {
